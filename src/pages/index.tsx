e7ba5cba3ef89a3e3d765d17ddebba1d9492c35e
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-06-28 07:52:40 -0500

    Pagina Sing In

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,62 @@
-import Head from "next/head"
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react"
 
 export default function Home() {
-    return(
-        <h1>Dashgo</h1>
+    //criando e configurando input e os botões
+    return (
+        <Flex
+            w="100vw"
+            h="100vh"
+            align="center"
+            justify="center"
+        >
+            <Flex
+                as="form"
+                width="100%"
+                maxWidth={360}
+                bg="gray.800"
+                p="8"
+                borderRadius={8}
+                flexDir="column"
+            >
+                <Stack spacing="8">
+                    <FormControl>
+                        <FormLabel htmlFor="email">E-mail</FormLabel>
+
+                        <Input 
+                            name="email" 
+                            type="email" 
+                            focusBorderColor="pink.500"
+                            bgColor="gray.900"
+                            variant="filled"
+                            _hover={{
+                                bgColor: "gray.900"
+                            }}
+                            size="lg"
+                        />
+                    </FormControl>
+                    
+                    <FormControl>
+                        <FormLabel htmlFor="password">Senha</FormLabel>
+
+                        <Input 
+                            name="password" 
+                            type="password"
+                            id="password"
+                            mt="2"
+                            focusBorderColor="pink.500"
+                            bgColor="gray.900"
+                            variant="filled"
+                            _hover={{
+                                bgColor: "gray.900"
+                            }}
+                            size="lg"
+                        />
+                    </FormControl>
+
+                </Stack>    
+
+                <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+            </Flex>
+        </Flex>
     )
 }
